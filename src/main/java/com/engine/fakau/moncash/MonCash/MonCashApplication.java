6ac6e9a8fa8ffa84e71b156060470da717fb6aa9
Laurent Kafka M <laurentkafka123@gmail.com>
2020-10-05 13:42:23 -0400

    fix, paiement

--- src/main/java/com/engine/fakau/moncash/MonCash/MonCashApplication.java
+++ src/main/java/com/engine/fakau/moncash/MonCash/MonCashApplication.java
@@ -1,6 +1,7 @@
 package com.engine.fakau.moncash.MonCash;
 
 import org.apache.http.HttpStatus;
+import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -17,7 +18,7 @@ import com.digicelgroup.moncash.payments.TransactionId;
 
 @SpringBootApplication
 public class MonCashApplication {
-	private static Logger logger = Logger.getLogger(MonCashApplication.class);
+	private static final Logger logger = Logger.getLogger(MonCashApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonCashApplication.class, args);
@@ -25,22 +26,24 @@ public class MonCashApplication {
 	
 	@Bean
 	public void init(){
-		APIContext apiContext = new APIContext("client id", "client secret", Constants.SANDBOX);
+		BasicConfigurator.configure();
+		APIContext apiContext = new APIContext(Utility.clientId, Utility.clientSecret, Constants.SANDBOX);
 		createPaiement( apiContext);
-		retrievePaiement(apiContext);
-		retrievePaiementByOrder(apiContext);
+		retrievePaiementByTransactionId(apiContext, "2155149110");
+		retrievePaiementByOrder(apiContext, "1601915231376");
 	} 
 	private void createPaiement(APIContext apiContext){
 		try{
 			PaymentCreator paymentCreator = new PaymentCreator();
 			Payment payment = new Payment();
-			payment.setOrderId(System.currentTimeMillis()+"");
+			payment.setOrderId(String.valueOf(System.currentTimeMillis()));
 			payment.setAmount(50);
 			PaymentCreator creator = paymentCreator.execute(apiContext,PaymentCreator.class, payment);
-			if(creator.getStatus() !=null && creator.getStatus().compareTo(HttpStatus.SC_ACCEPTED+"")==0){
+			if(creator.getStatus() !=null && creator.getStatus().compareTo(String.valueOf(HttpStatus.SC_ACCEPTED))==0){
 			  logger.info("redirect to the link below");
-			  System.out.println(creator.redirectUri());   // method return the payment gateway url
-			  logger.info(creator.redirectUri());
+			  String  redirectURl = creator.redirectUri();
+			  System.out.println(redirectURl);   // method return the payment gateway url
+			  logger.info(redirectURl);
 			}else if(creator.getStatus()==null){
 			  logger.error("Error");
 			  logger.error(creator.getError());
@@ -56,39 +59,39 @@ public class MonCashApplication {
 			System.out.println("CreatePaiementException: "+ex.getMessage());
 		}
 	}
-	private void retrievePaiement(APIContext apiContext){
+	private void retrievePaiementByTransactionId(APIContext apiContext, final String noTransaction){
 		try{
 			PaymentCapture paymentCapture = new PaymentCapture();
 			TransactionId transactionId = new TransactionId();
-			transactionId.setTransactionId("12874819");
+			transactionId.setTransactionId(noTransaction);
 			PaymentCapture capture = paymentCapture.execute(apiContext,PaymentCapture.class,transactionId);
-			if(capture.getStatus() !=null && capture.getStatus().compareTo(HttpStatus.SC_OK+"")==0){
+			if(capture.getStatus() !=null && capture.getStatus().compareTo(String.valueOf(HttpStatus.SC_OK))==0){
 			 logger.info("Transaction");
 			 logger.info(capture.getPayment().getMessage());
-			 logger.info("transactio_id="+capture.getPayment().getTransaction_id());
+			 logger.info("transactio_id = "+capture.getPayment().getTransaction_id());
 			 logger.info("Payer="+capture.getPayment().getPayer());
-			 logger.info("amount="+capture.getPayment().getCost()+"");
+			 logger.info("amount = "+capture.getPayment().getCost());
 			} else {
 			  logger.info(capture.getStatus());
 			}
 		}catch(Exception ex){
-			System.out.println("RetrievePaiementException"+ex.getMessage());
+			System.out.println("RetrievePaiementException "+ex.getMessage());
 		}
 
 	}
 	
-	private void retrievePaiementByOrder(APIContext apiContext){
+	private void retrievePaiementByOrder(APIContext apiContext, final String orderReference){
 		try{
 			PaymentCapture paymentCapture = new PaymentCapture();
 			OrderId orderId = new OrderId();
-			orderId.setOrderId("12874819");
+			orderId.setOrderId(orderReference);
 			PaymentCapture capture = paymentCapture.execute(apiContext,PaymentCapture.class, orderId);
-			if(capture.getStatus() !=null && capture.getStatus().compareTo(HttpStatus.SC_OK+"")==0){
+			if(capture.getStatus() !=null && capture.getStatus().compareTo(String.valueOf(HttpStatus.SC_OK))==0){
 			 logger.info("Transaction");
 			 logger.info(capture.getPayment().getMessage());
-			 logger.info("transactio_id="+capture.getPayment().getTransaction_id());
-			 logger.info("Payer="+capture.getPayment().getPayer());
-			 logger.info("amount="+capture.getPayment().getCost()+"");
+			 logger.info("transactio_id = "+capture.getPayment().getTransaction_id());
+			 logger.info("Payer = "+capture.getPayment().getPayer());
+			 logger.info("amount = "+capture.getPayment().getCost());
 			} else {
 			  logger.info(capture.getStatus());
 			}
@@ -97,6 +100,5 @@ public class MonCashApplication {
 		}
 
 	}
-	
 
 }
---
+++ src/main/java/com/engine/fakau/moncash/MonCash/Utility.java
@@ -0,0 +1,7 @@
+package com.engine.fakau.moncash.MonCash;
+
+public class Utility {
+    public static final String key = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String clientId = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+    public static final String clientSecret = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+}
